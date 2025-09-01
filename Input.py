61cc4321389e3f4836e09d2ee9ea2a11a3c5f426
Lokesh <sareddylokeshreddy@gmail.com>
2025-09-02 00:12:51 +0530

    new input is added

--- Input.py
+++ Input.py
@@ -1,2 +1,32 @@
 n="Hello world"
 print(n)
+
+#sum of two numbers
+a=10
+b=20
+c=a+b
+print(c)
+
+#add two strings
+a="Lokesh"
+b="Reddy"
+c=a+b
+print(c)
+
+#multiply two numbers
+a=10
+b=30
+c=a*b
+print(c)
+
+#divide two numbers
+a=30
+b=10
+c=a/b
+print(c)
+
+#substract two numbers
+a=30
+b=20
+c=a-b
+print(c)
