ac576cdc44209c0cdf7c36cd17f707e507ba830d
Lokesh <sareddylokeshreddy@gmail.com>
2025-09-02 21:59:59 +0530

    new problem

--- Input.py
+++ Input.py
@@ -30,3 +30,18 @@ a=30
 b=20
 c=a-b
 print(c)
+
+# taking two inputs ata time
+x,y = input("Enter two values: ").split()
+print("Number of boys: ",x)
+print("Number of girls: ",y)
+
+#find data type of input in python
+a="hello world"
+b=10
+c=10.2
+d="Geeks for Geeks"
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
