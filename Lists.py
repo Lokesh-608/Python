bdb756661dd5a2bb7aa84624d2424ed841ce15da
Lokesh <sareddylokeshreddy@gmail.com>
2025-09-10 00:07:26 +0530

    list problem

---
+++ Lists.py
@@ -0,0 +1,17 @@
+#list collection of unordered collection
+a = [1, 2, 3, 4, 5] 
+b = ['lokesh', 'varun', 'praneeth'] 
+c = [1, 'hello', 3.14, True] 
+print(a)
+print(b)
+print(c)
+#creating List with Repeated Elements
+a = [2] * 5
+b = [0] * 7
+print(a)
+print(b)
+#Accessing List Elements
+a = [10, 20, 30, 40, 50]
+print(a[0])    
+print(a[-1])
+print(a[1:4])   
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,4 @@
-x = 10
-y = 20
-print(x>>y)
+a = [10, 20, 30, 40, 50]
+print(a[0])    
+print(a[-1])
+print(a[1:4])   
