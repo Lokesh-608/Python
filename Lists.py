550c530eaeec295c9a289b826acc8c02cec6f013
Lokesh <sareddylokeshreddy@gmail.com>
2025-09-11 21:53:49 +0530

    problem

--- Lists.py
+++ Lists.py
@@ -41,3 +41,15 @@ print(i)
 a = [1,2,3,4]
 a.reverse()
 print(a)
+
+#common element
+a = [1,2,3,4,5,6]
+b = [2,5,6,7,8,9]
+c = list(set(a) & set(b))
+print(c)
+
+#sorting a list
+
+a = [1,2,7,3,4,5,6]
+a.sort()
+print(a)
