976a988e395e992df592cc3949f680a21b0c2c7b
Lokesh <sareddylokeshreddy@gmail.com>
2025-09-17 20:28:29 +0530

    problems

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,9 @@
-a = 50
-if a >= 40:
- print("passed")
- if a >= 75:
-   print("distinction")
+t = int(input("Enter temperature: "))
+if t < 20:
+  print("cold")
+  if 20 <= t <= 30:
+    print("pleasant")
+    if t > 30:
+      print("hot")
+
 
