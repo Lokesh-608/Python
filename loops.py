a141f5a4a915cd13371358fdb83fa8adbe4ed310
Lokesh <sareddylokeshreddy@gmail.com>
2025-09-23 22:44:02 +0530

    problems

--- loops.py
+++ loops.py
@@ -23,3 +23,15 @@ for j in i:
 
 for x in range(2, 12, 3):
   print(x)
+  #nested loop
+
+for i in range(1, 5):
+    for j in range(i):
+        print(i, end=' ')
+    print()
+
+for i in range(1, 5):
+    for j in range(i):
+        print(i, end=' ')
+        print(j, end=' ')
+        print()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,5 @@
-for x in range(2, 12, 3):
-  print(x)
+for i in range(1, 5):
+    for j in range(i):
+        print(i, end=' ')
+        print(j)
+        print()
