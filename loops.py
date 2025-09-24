e43573ecb02546f2a6fa83a8a2161677243108ac
Lokesh <sareddylokeshreddy@gmail.com>
2025-09-24 17:47:12 +0530

    problems

--- loops.py
+++ loops.py
@@ -32,6 +32,8 @@ for i in range(1, 5):
 
 for i in range(1, 5):
     for j in range(i):
-        print(i, end=' ')
-        print(j, end=' ')
+        print(i, end=' ') 
         print()
+        #hello
+for i in range(5):
+    print("Lokesh")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,2 @@
-for i in range(1, 5):
-    for j in range(i):
-        print(i, end=' ')
-        print(j)
-        print()
+for i in range(5):
+    print("Lokesh")
