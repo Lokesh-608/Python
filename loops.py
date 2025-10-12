e947dfaa8d1e4a0dbec397cb5ca9ba0c5ee49e0e
Sareddy Lokesh Reddy <sareddylokeshreddy@gmail.com>
2025-10-12 23:59:01 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -62,9 +62,4 @@ for i in range(5):
     for j in range(0,i+1):
         print("*", end="")  
     print()
-#number triangle
-n = 5
-for i in range(1, n + 1):
-    for j in range(2, i + 1):
-        print(j, end="")
-    print()
+#
