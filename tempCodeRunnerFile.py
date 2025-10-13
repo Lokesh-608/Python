c5a5c366481aaa2cf1ca9b85a437e7e51a862608
Lokesh <sareddylokeshreddy@gmail.com>
2025-10-13 21:22:51 +0530

    patterns

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,6 @@
-n = 5
-for i in range(1, n + 1):
-    for j in range(65, 65 + i):
-        print(chr(j), end="")
+for i in range(0,5):
+    for j in range(0,5):
+        if(j<=i):
+            print(j+1,end="")
     print()
+        
