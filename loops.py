400120adb29426998aea95d795d9ce9d27f5c4b5
Lokesh <sareddylokeshreddy@gmail.com>
2025-10-14 22:54:59 +0530

    patterns

--- loops.py
+++ loops.py
@@ -91,4 +91,14 @@ for i in range(n, 0, -1):
         if(j>=i):
             print(j, end="")
     print()
-        
+#inverted triangle
+#*****
+#****
+#***
+#**
+#*
+n = 5
+for i in range(n,0,1):
+    for j in range(0,i):
+        print("*", end="")
+    print()
