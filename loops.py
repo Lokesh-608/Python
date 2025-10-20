e24df47061645ae98a26896d4e1fa7d5cad99042
Sareddy Lokesh Reddy <sareddylokeshreddy@gmail.com>
2025-10-20 22:20:41 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -156,3 +156,11 @@ for i in range(1, n+1):
         print(j, end='')
     print()
     )
+#*
+   #***
+  #*****
+ #*******
+#*********
+n = 5
+for i in range(n):
+    print(" " * (n - i - 1) + "*" * (2*i + 1)
