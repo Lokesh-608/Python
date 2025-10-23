d0202653881068e2376d1a8f0896c1dd9453a8ae
Sareddy Lokesh Reddy <sareddylokeshreddy@gmail.com>
2025-10-23 22:14:07 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -122,17 +122,6 @@ n = 6
 for i in range(n, 0, -2):
     print('a' * i)
 print()
-#1
-#12
-#123
-#1234
-#12345
-n = 5
-for i in range(1, n+1):
-    for j in range(1, i+1):
-        print(j, end='')
-    print()
-    )
 #****
 #****
 #****
