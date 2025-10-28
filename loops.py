fc87a6a4f6552cba3ae77cfff570eb40e6a6d633
Sareddy Lokesh Reddy <sareddylokeshreddy@gmail.com>
2025-10-28 22:24:02 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -14,12 +14,7 @@ for i in t:
 l = "lokesh"
 for i in l:
     print(i)
-#break
-i = [1,2,3,4]
-for j in i:
-    print(j)
-    if j == 3:
-        break
+
 
 for x in range(2, 12, 3):
   print(x)                                                                                                                                                                                                        XZ
