9077e779560f744ae8ead9e1376e70569d1b6556
Sareddy Lokesh Reddy <sareddylokeshreddy@gmail.com>
2025-10-29 22:09:02 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -180,3 +180,11 @@ for i in range(n):
 #  321
 # 4321
 #54321
+n = 5
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end="")
+    for j in range(n):
+                if j<=i:
+                    print(i-j+1,end="")
+    print()
