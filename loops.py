29d8838bd80654c27bd88d836e07a8e9b899ad9f
Sareddy Lokesh Reddy <sareddylokeshreddy@gmail.com>
2025-11-03 22:20:13 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -26,7 +26,10 @@ for x in range(2, 12, 3):
   #nested loop
 
 
-
+for i in range(1, 5):
+    for j in range(i):
+        print(i, end=' ')
+    print()
 for i in range(1, 5):
     for j in range(i):
         print(i, end=' ') 
