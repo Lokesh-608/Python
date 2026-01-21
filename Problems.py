885812f9922eba8e41ca0e599efd00ecee6e95c8
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-01-21 20:27:02 +0530

    palindrome

--- Problems.py
+++ Problems.py
@@ -1,9 +1,21 @@
 #Check whether a number is Prime
-def is_prime(num):
-    if num<=1:
-        return False
-    for i in range(2,num):
-        if num%i==0:
-            return True
-        return False
-    
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print("It is not prime")
+else:
+    for i in range(2, num):
+        if num % i == 0:
+            print("It is not prime")
+            break
+    else:
+        print("It is prime")
+#Check Palindrome Number
+num =int(input("Enter the number: "))
+n = str(num)
+if n == n[::-1]:
+    print("It is palindrome")
+else:
+    print("It is not palindrome")
+     
+
