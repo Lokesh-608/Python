eed046f3a20d6a48f192e21cd33f9dd9c06cbe26
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-01-23 18:45:25 +0530

    fibonaci

--- Problems.py
+++ Problems.py
@@ -91,6 +91,12 @@ s=0
 for i in range(1,n+1):
     s=s+i
 print(s)
-
+#check even or odd
+n=int(input("Enter the number: "))
+if n%2==0:
+    print("It is even")
+else:
+    print("It is odd")
+ 
 
 
