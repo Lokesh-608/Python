7f85609bf308a81ccd8e197597218daaba056307
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-01-29 23:45:01 +0530

    leap year

--- Problems.py
+++ Problems.py
@@ -214,5 +214,7 @@ class Solution:
                 return False
         return True
 #leap year
-
+y=int(input("Enter the year: "))
+if(y%4==0 and y%100!=0) or (y%400==0):
+    print("It is leap year")
 
