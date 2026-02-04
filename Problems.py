23d02886912c575767ab325f0088d0c592e98988
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-02-04 23:16:15 +0530

    two sum

--- Problems.py
+++ Problems.py
@@ -322,6 +322,12 @@ for i in range(n):
         print("*",end=" ")
 print()
 #Two sum
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
 
 
    
