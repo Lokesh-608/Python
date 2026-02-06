dcaf913d23ef0313066acdd98fd58b0f8c4d6bee
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-02-06 23:37:38 +0530

    Two sum2

--- Problems.py
+++ Problems.py
@@ -343,3 +343,16 @@ if n>0 and (n & (n-1))==0:
 else:
     print("It is not power of two")
 #Two Sum II - Input Array Is Sorted
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers) - 1
+        
+        while left < right:
+            s = numbers[left] + numbers[right]
+            
+            if s == target:
+                return [left + 1, right + 1]
+            elif s < target:
+                left += 1
+            else:
+                right -= 1
