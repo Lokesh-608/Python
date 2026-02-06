160ea87e3d1be19b23c196f6f13f1782ee7704fa
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-02-06 23:42:09 +0530

    pattern

--- Problems.py
+++ Problems.py
@@ -356,3 +356,12 @@ class Solution:
                 left += 1
             else:
                 right -= 1
+#*
+#**
+#***
+#****
+n=int(input("Enter the number: "))
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
