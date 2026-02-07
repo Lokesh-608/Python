b5f3ee21cd8ae6933bc2364385464a0b8c36dd14
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-02-07 21:35:32 +0530

    new

--- Problems.py
+++ Problems.py
@@ -369,3 +369,11 @@ for i in range(n):
 #.***
 #. **
 #.  *
+n=int(input("Enter the number: "))
+for i in range(n):
+    for j in range(n):
+        if j<n-i-1:
+            print(" ",end=" ")
+        else:
+            print("*",end=" ")
+    print() 
